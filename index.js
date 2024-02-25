25ec1e97d9467c8d674684ff933e2364c0e75515
Mahendar Dwi Payana <hendartea@gmail.com>
2024-02-25 19:37:13 +0700

    tambah fitri

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const bot = new Bot(process.env.BOT_TOKEN);
 
 bot.command("start", (ctx) => ctx.reply("Selamat Datang di Bot Hendartea!"));
 bot.command("info", (ctx) => ctx.reply("Hai, Saya adalah bot-nya Bapak Mahendar Dwi Payana (hendartea_bot_telegram)"));
+bot.command("fitri", (ctx) => ctx.reply("Hai Fitri istri majikan yang paling dicintainya...❤️ ️"))
 // bot.on("message", (ctx) => ctx.reply("Got another message!"));
 bot.on("message", async (ctx) => {
     // Get the text of the message.
